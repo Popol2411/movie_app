33bba0b020ac4ee5ca089a09da7b850edba6e91a
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-02-13 21:10:47 +0100

    .

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+
+http
+  .createServer((request, response) => {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+          response.end("Test\n");
+            })
+              .listen(8080);
+
+              console.log("Node test server is running on Port 8080.");
+
+  fs = require('fs'),
+
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+        console.log(err);
+      } else {
+        console.log('Added to log.');
+      }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+        throw err;
+      }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
+  });
+
+}).listen(8080);
+console.log('My test server is running on Port 8080.');
