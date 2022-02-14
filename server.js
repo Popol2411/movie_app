214222fc5875471b8288428b1a827c5c6c0fbf42
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-02-14 09:55:22 +0100

    Comments added to server.js file

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const http = require("http");
+const http = require("http"); //cons(tant) http stating that http protocol is requested
 
-(fs = require("fs")), (url = require("url"));
+(fs = require("fs")), (url = require("url")); //fs -> file system
 
 http
   .createServer((request, response) => {
+    // http request creates the server and a response
     let addr = request.url,
       q = url.parse(addr, true),
       filePath = "";
 
     fs.appendFile(
+      // whenever a URL is opened, the address will be logged in the file log.txt including a timestamp and the date
       "log.txt",
-      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n", // \n -> breakpoint
       err => {
         if (err) {
-          console.log(err);
+          console.log(err); // if an error occurs, an "err" (error) will be thrown, otherwise "Added to log" would be display in the console
         } else {
           console.log("Added to log.");
         }
@@ -23,7 +25,7 @@ http
     if (q.pathname.includes("documentation")) {
       filePath = __dirname + "/documentation.html";
     } else {
-      filePath = "index.html";
+      filePath = "index.html"; // if the filename includes "documentation", the file documentation.html will open, otherwise the user will be redirected to index.html
     }
 
     fs.readFile(filePath, (err, data) => {
@@ -33,7 +35,7 @@ http
 
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(data);
-      response.end();
+      response.end(); // the file documentation.html is beeing read, otherwise error is thrown
     });
   })
   .listen(8080);
