2f67d1844544111a642315c16cb780a59128c769
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-03-15 00:18:10 +0100

    bcrypt added

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt'); //Module to compare hashed passwords
+
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
   Description: {type: String, required: true},
@@ -22,6 +24,14 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
+
+userSchema.statics.hashPassword = (password) => { //Hash submitted password
+  return bcrypt.hashSync(password, 10);  
+};
+
+userSchema.methods.validatePassword = function(password) {  //Compare submitted and validated passwords
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
